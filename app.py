9f819671d6e07eddc34de9163eddebe489d8d9f8
AliAnderson <alexis.anderson@target.com>
2020-08-25 17:54:06 -0500

    build flask server

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -10,6 +10,7 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+session = Session(engine)
 
 
 # Start Flask app
@@ -17,13 +18,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # List available routes
-    routes = "/api/v1.0/precipitation <br>" + \
-            "/api/v1.0/stations <br>" + \
-            "/api/v1.0/tobs <br>" + \
-            "/api/v1.0/<start> <br>" + \
-            "/api/v1.0/<start>/<end> <br>"
-    return f"Available Routes: <br> {routes}"
+    return render_template('index.html')
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Query precipitation
+    last_year_rows = session.query(Measurement.prcp, Measurement.date).filter(Measurement.date >= '2016-08-23') \
+                            .order_by(Measurement.date.asc()).all()
+                            
+    #Convert the query results to a dictionary using date as the key and prcp as the value.
+    prcp_dict = {}
+    for row in last_year_rows:
+        prcp_dict[row.date] = row.prcp
+
+    #Return the JSON representation of your dictionary.
+    return jsonify(prcp_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
